354a9eeb5f3d91572f436acb8ea21d19ec3d40d4
madhumita walawalkar <madhumitaw@gmail.com>
2017-06-08 06:52:40 -0700

    KAYAK:appium automation

---
+++ src/Demo/InvalidLoginEmail.java
@@ -0,0 +1,100 @@
+package Demo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class InvalidLoginEmail {
+	private static AndroidDriver<WebElement> driver;
+
+	private static Properties testData;
+public void setUp() throws FileNotFoundException, IOException{
+	
+	DesiredCapabilities capabilities=DesiredCapabilities.android();
+	
+	capabilities.setCapability(CapabilityType.BROWSER_NAME,"");
+	capabilities.setCapability("deviceName", "MyAndroid");
+	capabilities.setCapability("platformVersion", "5.0.2");
+	capabilities.setCapability("platformName","Android");
+
+	testData = new Properties();
+    testData.load(new FileInputStream("testdata/credentials.properties"));
+    capabilities.setCapability("appPackage","com.kayak.android");
+    capabilities.setCapability("appActivity","com.kayak.android.Splash");
+    driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	/*
+	 * Written By:Madhumita
+	 * Date:20th Nov 2016
+	 * Test For Invalid Login using email-log in  option
+	 */
+	
+	@Test
+    public void testInvalidLogin() {
+		driver.findElementByName("Sign in with your email").click();
+		//read number of test scripts to be executed
+		int noOfTests = Integer.parseInt(testData.getProperty("noofscripts"));
+		System.out.println("TestFor Invalid-Login");
+		System.out.println("Total Test Cases::"+noOfTests);
+		while(noOfTests > 0){
+			System.out.println("***************************************");
+			System.out.println("TEST CASE:"+noOfTests);
+			//Set the input field for email from the properties file
+			System.out.println("USERNAME:"+testData.getProperty("name"+noOfTests));
+			driver.findElementByName("Save up to 35% with hotel deals").sendKeys(testData.getProperty("name"+noOfTests));
+			//Set the input field for Password from the properties file
+			System.out.println("PASSWORD:"+testData.getProperty("pass"+noOfTests));
+			driver.findElementByName("Get flight status alerts").sendKeys(testData.getProperty("pass"+noOfTests));
+			//Set the preference for email option(yes /no)
+			System.out.println("EMAIL PREFERENCE:"+testData.getProperty("email"+noOfTests));
+			String email=testData.getProperty("email"+noOfTests);
+		
+			String ischecked=driver.findElementById("com.kayak.android:id/preferences_signup_deal").getAttribute("checked");
+			//Uncheck the default selected "Email me deals" if the value is set as no in the properties file
+			if(email.equalsIgnoreCase("no") && ischecked.equals("true"))
+				{
+			
+				driver.findElementById("com.kayak.android:id/preferences_signup_deal").click();
+				
+				}
+			//Check the default selected "Email me deals" if the value is set as yes in the properties file
+			if(email.equalsIgnoreCase("yes") && ischecked.equals("false"))
+				{
+			
+				driver.findElementById("com.kayak.android:id/preferences_signup_deal").click();
+				
+				}
+			//hide Keyboard
+			driver.hideKeyboard();
+			driver.findElementByName("Sign Up").click();
+			String msg=driver.findElement(By.id("android:id/message")).getText().toString();
+			System.out.println("Invalid Error messsage:"+msg);
+			
+			driver.findElementByName("OK").click();
+			System.out.println("In-Valid Login Test Pass for credentials:"+noOfTests);
+			System.out.println("***************************************");
+			noOfTests--;
+		}
+	}
+	
+	@After
+    public void closureActivities(){
+        driver.quit();
+    }
+	}
+	
+
---
+++ src/Demo/InvalidLoginFacebook.java
@@ -0,0 +1,85 @@
+package Demo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class InvalidLoginFacebook {
+	private static AndroidDriver<WebElement> driver;
+
+	private static Properties testData;
+	@Before
+    public void setUp() throws FileNotFoundException, IOException{
+	
+DesiredCapabilities capabilities=DesiredCapabilities.android();
+	
+	capabilities.setCapability(CapabilityType.BROWSER_NAME,"");
+	capabilities.setCapability("deviceName", "MyAndroid");
+	capabilities.setCapability("platformVersion", "5.0.2");
+	capabilities.setCapability("platformName","Android");
+
+	testData = new Properties();
+    testData.load(new FileInputStream("testdata/credentials.properties"));
+    capabilities.setCapability("appPackage","com.kayak.android");
+    capabilities.setCapability("appActivity","com.kayak.android.Splash");
+    driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	/*
+	 * Written By:Madhumita
+	 * Date:20th Nov 2016
+	 * Test For InValid Login using Facebook  option
+	 */
+	
+	
+	@Test
+    public void testInvalidFBLogin() {
+		driver.findElementByName("Connect with Facebook").click();
+		System.out.println("Test For Invalid Facebook");
+		//driver.findElementByName("Connect with Facebook").click();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		int noOfTests = Integer.parseInt(testData.getProperty("noofscriptsIF"));
+		
+		//switch context to webview as this is an hybrid app supporting both the Native APP context and WEBVIEW aontext
+		driver.context("WEBVIEW");
+	
+		while(noOfTests > 0)
+			{
+			System.out.println("***************************************");
+			System.out.println("TEST CASE:"+noOfTests);
+			//set the facebook Login name or email id from the properties file
+			System.out.println("USERNAME:"+testData.getProperty("namefb"+noOfTests));
+			driver.findElement(By.name("email")).sendKeys(testData.getProperty("namefb"+noOfTests));
+			System.out.println("USERNAME:"+testData.getProperty("passfb"+noOfTests));
+			//set the facebook password name or email id from the properties file
+			driver.findElement(By.name("pass")).sendKeys(testData.getProperty("passfb"+noOfTests));
+			//hide keyboatd to select Login 
+			
+			driver.hideKeyboard();
+			//Click on Login 
+			driver.findElement(By.name("login")).click();
+			System.out.println("In-Valid Login Test Pass for Facebook credentials:"+noOfTests);
+			System.out.println("***************************************");
+			noOfTests--;
+		}
+	}
+	
+	@After
+    public void closureActivities(){
+        driver.quit();
+    }
+}
